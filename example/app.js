cb0a3c7b3b863cf280666e7f5ed5850ff29bac25
MatthewJon <matthew.drooker@gmail.com>
2016-01-27 18:11:52 +0000

    Saved app.js file

--- example/app.js
+++ example/app.js
@@ -1,14 +1,13 @@
 'use strict';
 
-const logger = require('../main');
-const fileTransport = require('../main').winston.transports.File;
-
+const logger = require('../main'),
+    fileTransport = require('../main').winston.transports.File;
 
 logger.addHTTP('silly',  {host: 'g5vb4gr9s0yq.runscope.net', path:'/', level:'silly'});
-//logger.addUDP('silly',{server:'g5vb4gr9s0yq.runscope.net', port:'80'})
 logger.addTransport('silly', fileTransport, {filename:'temp', level:'silly'});
+logger.addUDP('silly',{server:'g5vb4gr9s0yq.runscope.net', port:'80'});
 
-setInterval(()=>{
+setInterval(() => { 
     logger.logger.silly('Silly');
-},2000);
+}, 1000 * 2 );
 
