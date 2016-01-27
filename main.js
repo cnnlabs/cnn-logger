60abeb97ec913bfff1e08b47e6ee1be969ae28b7
MatthewJon <matthew.drooker@gmail.com>
2016-01-27 11:06:12 +0000

    logger: update to add logstash support
    - added example app
    
    PR-URL: #1
    Reviewed-By: James Young <james.young@turner.com>
    Reviewed-By: Matthew Drooker <matthew.drooker@turner.com>

---
+++ main.js
@@ -0,0 +1,92 @@
+'use strict';
+
+const winston =require('winston'),
+    transports = require('./transports');
+let logger;
+require('winston-udp');
+
+class Logger {
+    constructor() {
+        this.logger = new (winston.Logger)();
+    }
+    addTransport(level, transport, opts){
+        this.logger.add(
+            transport,
+            Object.assign({}, {
+                level: level || 'info'
+            }, opts)
+        );
+
+    }
+    addHTTP(level, opts){
+        if (this.logger.transports.http) {
+            return;
+        }
+        this.logger.add(
+            winston.transports.Http,
+            Object.assign({}, {
+                level: level || 'info'
+            }, opts)
+        );
+    }
+    addUDP(level, opts){
+        if (this.logger.transports.http) {
+            return;
+        }
+        this.logger.add(
+            winston.transports.UDP,
+            Object.assign({}, {
+                level: level || 'info'
+            }, opts)
+        );
+    }
+    removeUDP(){
+        if (!this.logger.transports.udp) {
+            return;
+        }
+        this.logger.remove('udp');
+    }
+    removeHTTP(){
+        if (!this.logger.transports.http) {
+            return;
+        }
+        this.logger.remove('http');
+    }
+    addConsole(level, opts) {
+        if (this.logger.transports.console) {
+            return;
+        }
+        this.logger.add(
+            winston.transports.Console,
+            Object.assign({}, {
+                level: level || 'info'
+            }, opts)
+        );
+    }
+    removeConsole() {
+        if (!this.logger.transports.console) {
+            return;
+        }
+        this.logger.remove('console');
+    }
+
+    clearLoggers() {
+        Object.keys(this.logger.transports)
+        .forEach(logger => this.logger.remove(logger));
+    }
+    transports(){
+        return transports;
+    }
+}
+
+
+let consoleLoggerLevel;
+if (process.env.NODE_ENV === 'test') {
+    consoleLoggerLevel = 'error';
+} else {
+    consoleLoggerLevel = process.env.CONSOLE_LOGGER_LEVEL || 'silly';
+}
+logger = new Logger;
+logger.addConsole(consoleLoggerLevel);
+module.exports= logger;
+module.exports.winston = winston;
