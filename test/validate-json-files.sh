197aaa7bd8c3ea5bd7f71ccd39806d37bc16c4d1
Jamie Young <jamsyoung@users.noreply.github.com>
2016-07-01 13:33:31 -0400

    rewrite (#4)
    rewrite: change of direction, rewritten

--- test/validate-json-files.sh
+++ test/validate-json-files.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Copyright 2016 Turner Broadcasting System, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # This can be done in one line, but the file that has the error is not captured
 #
 # find . -name "*.json" -not -path "./node_modules/*" -not -path "./test/*" -not -path "./ruby/*" | xargs -n1 node_modules/.bin/jsonlint -q
@@ -14,7 +28,6 @@ EXIT_CODE=0
 JSON_FILES=(`find . \
                   -name "*.json"\
                   -not -path "./.c9/*"\
-                  -not -path "./.idea/*"\
                   -not -path "./node_modules/*"\
                   -not -path "./docs/*"\
             `)
